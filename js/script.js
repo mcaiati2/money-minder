d6270594e489f8793ee68486823a0e05647341b5
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-09-28 13:50:27 -0400

    calculated total expenses and added them to the header. need to adjust css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const expenseForm = document.querySelector('#expense-form');
 const expenseButton = document.querySelector('#expense-button');
-// const dropdownBtn = document.querySelector('#dropdown-button')
+const dropdownBtn = document.querySelector('#dropdown-button')
 const limitForm = document.querySelector('#limit-form');
 const limitButton = document.querySelector('#limit-button');
+const totalDisplay = document.querySelector('#total-display');
 
 function getExpenses() {
     return JSON.parse(localStorage.getItem('storedExpenses')) || [];
@@ -10,6 +11,10 @@ function getExpenses() {
 
 function getLimitAmount() {
     return JSON.parse(localStorage.getItem('storedLimitAmount')) || 0;
+}
+
+function getTotalExpenseAmount() {
+    return JSON.parse(localStorage.getItem('storedTotalExpenses')) || 0;
 }
 
 function addExpense() {
@@ -24,7 +29,7 @@ function addExpense() {
 
     const expenseObject = {
         category: categoryInput,
-        amount: amountInput,
+        amount: ('$' + amountInput),
         date: dateInput,
     };
 
@@ -32,6 +37,7 @@ function addExpense() {
 
     localStorage.setItem('storedExpenses', JSON.stringify(expenseArray));
     showTable();
+    showTotalExpenses();
 
     console.log('New expense added! Here are the details: ', expenseObject);
     
@@ -54,6 +60,36 @@ function addLimitAmount() {
 
 }
 
+function calculateTotalExpenses() {
+    let expenseArray = getExpenses();
+
+    // Extract amounts and convert them to numbers
+    let total = expenseArray.reduce((sum, expense) => {
+        return sum + parseFloat(expense.amount.replace('$', ''));
+    }, 0);
+    localStorage.setItem('storedTotalExpenses', JSON.stringify(total));
+
+    console.log(total, 'is the total amount of expenses.');
+    return total;
+
+    
+}
+
+function showTotalExpenses() {
+    let storedTotalExpenses = calculateTotalExpenses();
+
+    let displayedTotal = document.querySelector('#total-display');
+
+    displayedTotal.innerHTML = '';
+
+    let displayedTotalHTML = `
+    <h2 class="total-display">Total Expenses:
+    $${storedTotalExpenses}</h3>
+    `;
+    displayedTotal.insertAdjacentHTML('beforeend', displayedTotalHTML);
+
+}
+
 function showLimitAmount() {
 
     let storedLimitAmount = getLimitAmount();
@@ -68,8 +104,6 @@ function showLimitAmount() {
     `;
     displayedLimit.insertAdjacentHTML(
     'beforeend', displayedLimitHTML);
-
-    
 
 }
 
@@ -121,6 +155,7 @@ function showTable() {
 // The main buttons exist in the HTML! That's why there's no reference to them here!
 
 function initialize() {
+    // dropdownBtn.addEventListener('click', );
     expenseForm.addEventListener('submit', addExpense);
 
     // TODO why do we need click?
@@ -138,6 +173,7 @@ function initialize() {
     $('#date-input').datepicker();
     showTable();
     showLimitAmount();
+    showTotalExpenses();
 
 
     /* 
